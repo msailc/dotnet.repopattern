d3c4c340a956dd951fbf9c417945b7cc8a80d6e5
Mustafa <mustafaislamovich@gmail.com>
2022-12-14 18:12:22 +0100

    made simple login,register and JWT token creation logic

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -7,11 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-
-    [ApiController]
-    [Route("api/[controller]")]
-
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
